6cdea39d71f2a00b78fc16a9fb0a3ea005a34530
terence-uzamere <otuzamere@quinnipiac.edu>
2015-11-03 19:07:00 +0000

    Add try create view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
 
 # Create your views here.
 class Home(TemplateView):
   template_name = 'home.html'
+
+class TryCreateView(CreateView):
+  model = Try
+  template_name = 'try/try_form.html'
+  fields = ['category', 'caption']
+  success_url = reverse_lazy('home')
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super(TryCreateView, self).form_valid(form)
